8a79cc0b18033f28a69f15d9d1f74336ad470e41
Ronald Ibañez <ronald_reir21@hotmail.com>
2018-06-23 14:35:56 -0600

    Se arreglo el Get nueva url
    localhost:3000/marcas
    
    iniciar con $nodemon app.js

---
+++ ApiMarca/app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const mysql = require('mysql');
+
+//crear conexion
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'mantenimiento',
+    port: '3300'
+
+});
+
+//Conectar
+db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log("Mysql Conectado...");
+});
+
+const app = express();
+
+
+app.get('/createdb',(req, res) =>{
+    let sql = 'CREATE DATABASE mantenimiento2';
+    db.query(sql, (err, result) =>{
+        if(err) throw err;
+        console.log(result);
+        res.send('Database created...');
+    });
+});
+
+app.get('/marcas', (req, res) => {
+   let sql = 'SELECT * FROM marca';
+   let query = db.query(sql, (err, results) => {
+       if(err) throw err;
+       console.log(results);
+       res.status(200).json(results);
+   });
+});
+
+
+app.listen('3000', () =>
+    {
+        console.log('Server inicio en el puerto 3000')
+    }
+
+);
