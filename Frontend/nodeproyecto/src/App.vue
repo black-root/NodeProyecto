c62b41906f1ff8352c9a19352ec8ebb57b55e54c
sergio9508 <menjivarsergio@gmail.com>
2018-06-25 03:01:11 -0600

    docker

--- Frontend/nodeproyecto/src/App.vue
+++ Frontend/nodeproyecto/src/App.vue
@@ -3,7 +3,7 @@
             <div id="content" class="ui basic segment">
                 <h3 class="ui header">List of Users</h3>
                 <vuetable
-                    api-url="http://localhost:3000/marcas"
+                    api-url="http://172.25.0.30:3000/marcas"
                     table-wrapper="#content"
                     :fields="columns"
                     :item-actions="itemActions"
